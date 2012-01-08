7047d0b81ea2fa5916d2d70404e2329bc51be87e
Ozan Turgut <ozanturgut@gmail.com>
2012-01-08 12:12:40 -0800

    Posts are now either all HTML or just a redirect.

--- posts/models.py
+++ posts/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from datetime import datetime
 
 class Post(models.Model):
-    title = models.CharField(max_length=64)	
-    date = models.DateTimeField(default=datetime.now())	
-    added = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=64)    
+    description = models.CharField(max_length=140)    
+    url = models.CharField(max_length=140)    
     head = models.TextField(blank=True)
     body = models.TextField()
-    visible = models.BooleanField(default=True)
+    html = models.TextField()
+    is_promoted_to_front_page = models.BooleanField(default=True)
+    date = models.DateTimeField(default=datetime.now())    
+    added = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(blank=True, upload_to='uploads/%Y/%m')
     slug = models.SlugField(primary_key=True, max_length=64)
 
