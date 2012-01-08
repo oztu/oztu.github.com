0efada0556984d14c8ebf27095901d887542d47a
Ozan Turgut <ozanturgut@gmail.com>
2012-01-08 12:12:40 -0800

    Posts are now either all HTML or just a redirect.

--- posts/views.py
+++ posts/views.py
@@ -5,9 +5,12 @@ import urllib2
 from django.http import HttpResponseRedirect, HttpResponse
 
 def index(request):
-    posts = Post.objects.filter(visible=True).order_by('-date')[:10]
+    posts = Post.objects.filter(promoted_to_front_page=True).order_by('-date')[:10]
     return render_to_response('posts/index.html', {'posts': posts})
 
 def post(request, slug):
     post = get_object_or_404(Post, pk=slug)
-    return render_to_response('posts/post.html', {'post': post})
+    if(len(post.url) == 0):
+        return HttpResponse(post.html)
+    else:
+        return redirect(post.url, permanent=True)
