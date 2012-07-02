5e82d155c73f53f37b2f651af58310800a660dc5
Ozan Turgut <ozanturgut@gmail.com>
2012-07-02 00:01:59 -0700

    Added two additional posts

---
+++ site/static/html/template/lib/scrollableCave.js
@@ -0,0 +1,73 @@
+var makeScrollableCave = function(selector, isVertical){
+	var scrollableCaves = $(selector);
+	var GUTTER_SIZE = 5; // pixels from an edge before activating caves
+	
+	if(isVertical){
+		scrollableCaves.append('<div class="caveShadowTop"></div><div class="caveShadowBottom"></div>');
+	} else {
+		scrollableCaves.append('<div class="caveShadowLeft"></div><div class="caveShadowRight"></div>');
+	}
+	
+	function setVerticalCaveShadows(wrapper, scroller){
+		var scrollTop = scroller.scrollTop();
+		var viewableHeight = scroller.outerHeight();
+		var scrollHeight = scroller[0].scrollHeight;
+		
+		if(scrollTop < GUTTER_SIZE){
+			wrapper.removeClass("inTopCave");
+		} else {
+			wrapper.addClass("inTopCave");
+		} 
+		
+		if(scrollTop + viewableHeight > scrollHeight - GUTTER_SIZE ){
+			wrapper.removeClass("inBottomCave");
+		} else {
+			wrapper.addClass("inBottomCave");
+		}
+	}
+	
+	function setHorizontalCaveShadows(wrapper, scroller){
+		var scrollLeft = scroller.scrollLeft();
+		var viewableWidth = scroller.outerWidth();
+		var scrollWidth = scroller[0].scrollWidth;
+		
+		if(scrollLeft < GUTTER_SIZE){
+			wrapper.removeClass("inLeftCave");
+		} else {
+			wrapper.addClass("inLeftCave");
+		} 
+		
+		if(scrollLeft + viewableWidth > scrollWidth - GUTTER_SIZE ){
+			wrapper.removeClass("inRightCave");
+		} else {
+			wrapper.addClass("inRightCave");
+		}
+	}
+
+	var setCaveShadows;
+	if(isVertical){
+		setCaveShadows = setVerticalCaveShadows
+	}else {
+		setCaveShadows = setHorizontalCaveShadows;
+	}
+	
+    scrollableCaves.each(function(index, item){
+		var scrollableCave = $(item);
+		var scroller = scrollableCave.find("ul");
+		scroller.scroll(function(){
+			setCaveShadows(scrollableCave, scroller);
+		});
+		
+		$(window).resize(function(){
+			setCaveShadows(scrollableCave, scroller);
+		});
+		
+		setCaveShadows(scrollableCave, scroller);
+	});
+};
+
+$(function(){
+	makeScrollableCave(".verticalScrollableCave", true);
+	makeScrollableCave(".horizontalScrollableCave", false);
+});
+
--- site/static/script/post_inject.js
+++ site/static/script/post_inject.js
@@ -26,6 +26,6 @@ _gaq.push( [ '_trackPageview' ]);
 	}
 	
 
-	var fragment = create('<h1 style="line-height:100%; font-size:100%; text-align:center; padding:0px; margin:0px; background-color:white; background-color:#eee; box-shadow:0px 0px 15px 0px #333; border-bottom:1px solid #ddd; margin-bottom:15px;"><a style="text-shadow:0 -1px 1px #AAA,0px 1px 2px #EEE; display:block; padding:8px; color:black; font-weight:normal; font-size:26px; font-family:Helvetica,Arial; line-height:100%" href="http://oztu.org/">Ozan Turgut\'s Website</a></h1>');
+	var fragment = create('<h1 style="line-height:100%; font-size:100%; text-align:center; padding:0px; margin:0px; background-color:white; background-color:#eee; box-shadow:0px 0px 15px 0px #333; border-bottom:1px solid #ddd; margin-bottom:15px;"><a style="text-shadow:0 -1px 1px #AAA,0px 1px 2px #EEE; display:block; padding:8px; color:black; font-weight:normal; font-size:26px; font-family:Helvetica,Arial; line-height:100%; text-decoration:none;" href="http://oztu.org/">Ozan Turgut\'s Website</a></h1>');
 	document.body.insertBefore(fragment, document.body.childNodes[0]);	
 }());
