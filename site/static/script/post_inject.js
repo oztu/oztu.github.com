b2050a0589fcd3b68dba5e41f9928242c69c8316
Ozan Turgut <ozanturgut@gmail.com>
2012-07-01 18:41:38 -0700

    Styling adjustments

--- site/static/script/post_inject.js
+++ site/static/script/post_inject.js
@@ -24,7 +24,8 @@ _gaq.push( [ '_trackPageview' ]);
 	    }
 	    return frag;
 	}
+	
 
-	var fragment = create('<h1 style="text-align:center; margin-top:10px; margin-bottom:20px;"><a style="text-shadow:0 -1px 1px #B0B0B0,0px 1px 2px #CCC; color:black; font-weight:normal; font-size:28px; font-family:Helvetica,Arial;" href="http://oztu.org/">Ozan Turgut\'s Website</a></h1>');
+	var fragment = create('<h1 style="line-height:100%; font-size:100%; text-align:center; padding:0px; margin:0px; background-color:white; background-color:#eee; box-shadow:0px 0px 15px 0px #333; border-bottom:1px solid #ddd; margin-bottom:15px;"><a style="text-shadow:0 -1px 1px #AAA,0px 1px 2px #EEE; display:block; padding:8px; color:black; font-weight:normal; font-size:26px; font-family:Helvetica,Arial; line-height:100%" href="http://oztu.org/">Ozan Turgut\'s Website</a></h1>');
 	document.body.insertBefore(fragment, document.body.childNodes[0]);	
 }());
