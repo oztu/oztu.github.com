c1b707642ba840872a236ac5b16f3ff7d24663c9
Ozan Turgut <ozanturgut@gmail.com>
2013-04-02 02:15:46 -0700

    Better animation support
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- script/pagesController.js
+++ script/pagesController.js
@@ -1,9 +1,13 @@
-var controller = module.exports = function($scope, $route, $rootScope){
+var controller = module.exports = function($scope, $route, $rootScope, $window){
 	$scope.pageTitle = "";
+	$scope.goHome = function(){
+		if(!$scope.pageTitle) return;
+		$window.location.hash = "/"
+	};
 	$rootScope.$on('$routeChangeSuccess', function(e, newRoute, oldRoute){
 		if(newRoute && newRoute.$route) $scope.pageTitle = newRoute.$route.title;
 		else $scope.pageTitle = ""
 	});
 
 };
-controller.$inject = ['$scope', '$route', '$rootScope'];
+controller.$inject = ['$scope', '$route', '$rootScope', '$window'];
