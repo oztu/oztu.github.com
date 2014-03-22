4cc6c515b4c8bec0e39a5baeee27f593fc371333
Ozan Turgut <ozanturgut@gmail.com>
2014-03-22 00:59:25 -0700

    updates
    Signed-off-by: Ozan Turgut <ozanturgut@gmail.com>

--- turn/turn-helper.js
+++ turn/turn-helper.js
@@ -1,11 +1,25 @@
 var styles = document.createElement('link');
 styles.src='https://oztu.github.io/turn/clippy.css';
 document.body.appendChild(styles);
-var script = document.createElement('script');
-script.src='https://oztu.github.io/turn/clippy.min.js';
-document.body.appendChild(script);
-script.onload = initTurnHelper;
+
+var jqueryScript = document.createElement('script');
+jqueryScript.src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js';
+document.body.appendChild(jqueryScript);
+
+jqueryScript.onload = loadClippy;
+
+function loadClippy(){
+	var script = document.createElement('script');
+	script.src='https://oztu.github.io/turn/clippy.min.js';
+	document.body.appendChild(script);
+	script.onload = initTurnHelper;
+}
+
 
 function initTurnHelper(){
-	alert('ready!');
+	 clippy.load('Clippy', function(agent) {
+        // Do anything with the loaded agent
+        agent.show();
+        window.a = agent;
+    });
 }
